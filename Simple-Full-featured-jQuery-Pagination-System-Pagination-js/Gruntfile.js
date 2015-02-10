d1d54bec911834b603485157bbff217316d73d98
Danny Glix <danny@dannyglix.com>
2015-02-10 18:13:51 +0530

    Simple Full featured jQuery Pagination System Pagination js

---
+++ Simple-Full-featured-jQuery-Pagination-System-Pagination-js/Gruntfile.js
@@ -0,0 +1,33 @@
+﻿// grunt
+module.exports = function(grunt){
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        uglify: {
+            options: {
+                banner: '/*\n' +
+                    ' * pagination.js <%= pkg.version %>\n' +
+                    ' * <%= pkg.description %>\n' +
+                    ' * https://github.com/superRaytin/paginationjs\n\n' +
+                    ' * Homepage: http://paginationjs.com\n' +
+                    ' *\n' +
+                    ' * Copyright 2014-2100, superRaytin\n' +
+                    ' * Released under the MIT license.\n' +
+                    '*/\n'
+            },
+            main: {
+                files: [
+                    {
+                        src: ['dist/pagination.js'],
+                        dest: 'dist/pagination.min.js'
+                    },
+                    {
+                        src: ['dist/pagination-with-styles.js'],
+                        dest: 'dist/pagination-with-styles.min.js'
+                    }
+                ]
+            }
+        }
+    });
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.registerTask('default', ['uglify']);
+};
